70765aaf5fbaabdab0aec7282fb65534bf9e6f89
simac <sima.chovancova@gmail.com>
2025-07-28 23:09:42 +0200

    Dockerize the API

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Kernel;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $number = random_int(0, 100);
+    return new Response(
+        '<html><body>Lucky number: '.$number.'</body></html>'
+    );
 };
